401b0748c51ce8a8c3c2f0e1d904b7754a09032d
JackieLiu <liutong6053@163.com>
2016-10-27 10:59:38 +0800

    添加灵云信息

---
+++ src/main/java/com/ai/opt/demo/web/service/HcicloudService.java
@@ -0,0 +1,9 @@
+package com.ai.opt.demo.web.service;
+
+/**
+ * Created by liutong on 16/10/27.
+ */
+public class HcicloudService {
+
+
+}
--- src/main/java/com/ai/opt/demo/web/servlet/HcicloudServlet.java
+++ src/main/java/com/ai/opt/demo/web/servlet/HcicloudServlet.java
@@ -1,9 +1,23 @@
 package com.ai.opt.demo.web.servlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * 灵云demo
  * Created by liutong on 16/10/26.
  */
-public class HcicloudServlet {
+public class HcicloudServlet extends HttpServlet{
+    private static final Logger LOGGER = LoggerFactory.getLogger(HcicloudServlet.class);
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
