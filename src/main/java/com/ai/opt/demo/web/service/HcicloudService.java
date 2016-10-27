bf2d70f450f4753ec83c2b7c047e04170b962e39
JackieLiu <liutong6053@163.com>
2016-10-27 20:49:52 +0800

    完善灵云调用

--- src/main/java/com/ai/opt/demo/web/service/HcicloudService.java
+++ src/main/java/com/ai/opt/demo/web/service/HcicloudService.java
@@ -1,10 +1,11 @@
 package com.ai.opt.demo.web.service;
 
-import java.io.IOException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -15,13 +16,17 @@ import org.slf4j.LoggerFactory;
 
 import  com.ai.opt.demo.web.utils.MD5Util;
 
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by liutong on 16/10/27.
  */
 public class HcicloudService {
+    public static File file = new File("tts.wav");
     private static String APPKEY = "ad5d5421";
     private static String DEVKEY = "bca4b0015b309b76301bb10efdf90561";
     private static String SENDURL = "http://test.api.hcicloud.com:8880/tts/SynthText";
+    private static final String PROXY_URL = "http://123.56.4.39:8180/demo-web/hclouts?forNum=1";
     private static final Logger LOGGER = LoggerFactory.getLogger(HcicloudService.class);
     
     /**
@@ -31,11 +36,12 @@ public class HcicloudService {
      * @return
      * @author mimw
      */
-    public String ttsSynth(String text) {
+    public String ttsSynth(String text,HttpServletResponse resp) {
         CloseableHttpClient client = HttpClients.createDefault();  
         HttpPost post = new HttpPost(SENDURL);
         CloseableHttpResponse response = null;
         String resStr = "";
+        InputStream ttsIn = null;
         
         SimpleDateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date date=new Date();
@@ -60,9 +66,16 @@ public class HcicloudService {
             long httpEnd = System.currentTimeMillis();
             LOGGER.info("结束httpClient,当前时间戳:{},用时:{}",httpEnd,(httpEnd-httpStart));
             HttpEntity entity = response.getEntity();
-            System.out.println(response.getStatusLine().getStatusCode());
-            resStr = EntityUtils.toString(entity, "UTF-8");
-            LOGGER.info(resStr);
+//            ttsIn = entity.getContent();
+//            byte[] bytes = new byte[1024*1024];
+//            int byteCount;
+//            while ((byteCount=ttsIn.read(bytes))>-1)
+//                resp.getOutputStream().write(bytes,0,byteCount);
+            //写入文件
+            intoFile(file,entity.getContent());
+//            System.out.println(response.getStatusLine().getStatusCode());
+//            resStr = EntityUtils.toString(entity, "UTF-8");
+//            LOGGER.info(resStr);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -75,6 +88,69 @@ public class HcicloudService {
             }
         }
         return resStr;
+    }
+
+    public void proxyTts(){
+        long startTime = System.currentTimeMillis();
+        LOGGER.info("开始 proxyTts,当前时间戳:{}",startTime);
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet post = new HttpGet(PROXY_URL);
+        CloseableHttpResponse response = null;
+        String resStr = "";
+
+        try {
+            long httpStart = System.currentTimeMillis();
+            LOGGER.info("开始httpClient,当前时间戳:{}",httpStart);
+            response = client.execute(post);
+            long httpEnd = System.currentTimeMillis();
+            LOGGER.info("结束httpClient,当前时间戳:{},用时:{}",httpEnd,(httpEnd-httpStart));
+            HttpEntity entity = response.getEntity();
+            SimpleDateFormat dateFormater = new SimpleDateFormat("HHmmss");
+            Date date=new Date();
+            String dataStr = dateFormater.format(date);
+            //写入文件
+            File file = new File(dataStr+"tts.wav");
+            intoFile(file,entity.getContent());
+//            System.out.println(response.getStatusLine().getStatusCode());
+//            resStr = EntityUtils.toString(entity, "UTF-8");
+//            LOGGER.info(resStr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (response != null) {
+                try {
+                    EntityUtils.consume(response.getEntity());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        LOGGER.info("开始 proxyTts,当前时间戳:{},用时:{}",endTime,(endTime-startTime));
+    }
+
+    private void intoFile(File file,InputStream inputStream){
+        try {
+            if (!file.exists())
+                file.createNewFile();
+            System.out.println(file.getAbsolutePath());
+            FileOutputStream fos = new FileOutputStream(file);
+            int byteCount = 0;
+            //1M逐个读取
+            byte[] bytes = new byte[1024*1024];
+            while ((byteCount = inputStream.read(bytes)) != -1){
+                fos.write(bytes, 0, byteCount);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        HcicloudService cloudService = new HcicloudService();
+//        cloudService.ttsSynth("今天天气很好");
     }
     
 }
--- src/main/java/com/ai/opt/demo/web/servlet/HcicloudServlet.java
+++ src/main/java/com/ai/opt/demo/web/servlet/HcicloudServlet.java
@@ -10,7 +10,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * 灵云demo
@@ -29,11 +31,15 @@ public class HcicloudServlet extends HttpServlet{
         if (proxy==null) {
             LOGGER.info("开始执行语音合成,当前时间:{}", startTime);
             HcicloudService hcicloudService = new HcicloudService();
-            
-            for (int i = 0; i < forNum; i++) {
-                hcicloudService.ttsSynth("今天天气很好");
-            }
-            
+            resp.setCharacterEncoding("UTF-8");
+            resp.setHeader("Content-type","text/html;charset=UTF-8");
+            hcicloudService.ttsSynth("今天天气很好",resp);
+            FileInputStream fis = new FileInputStream(HcicloudService.file);
+            byte[] bytes = new byte[1024*1024];
+            int byteCount;
+            while ((byteCount=fis.read(bytes))>-1)
+                resp.getOutputStream().write(bytes,0,byteCount);
+
             long endTime = System.currentTimeMillis();
             LOGGER.info("结束执行语音合成,当前时间:{},用时:{}", endTime, (endTime - startTime));
         }else
@@ -46,8 +52,9 @@ public class HcicloudServlet extends HttpServlet{
     private void proxy(int forNum){
         long startTime = System.currentTimeMillis();
         LOGGER.info("HcicloudServlet.代理开始执行翻译,当前时间:{}", startTime);
+        HcicloudService hcicloudService = new HcicloudService();
         for (int i = 0; i < forNum; i++) {
-            HttpUtil.doGet("http://123.56.4.39:8180/demo-web/hclouts?forNum=1");
+            hcicloudService.proxyTts();
         }
         long endTime = System.currentTimeMillis();
         LOGGER.info("HcicloudServlet.代理结束执行翻译,当前时间:{},用时:{}", endTime, (endTime - startTime));
