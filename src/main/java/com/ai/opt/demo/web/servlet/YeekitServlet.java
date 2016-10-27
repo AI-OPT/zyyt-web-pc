24deb7cd240f5330feb50f7e811e6c478174b781
JackieLiu <liutong6053@163.com>
2016-10-27 14:32:12 +0800

    添加proxy模式.

--- src/main/java/com/ai/opt/demo/web/servlet/YeekitServlet.java
+++ src/main/java/com/ai/opt/demo/web/servlet/YeekitServlet.java
@@ -1,6 +1,7 @@
 package com.ai.opt.demo.web.servlet;
 
 import com.ai.opt.demo.web.service.YeekitService;
+import com.ai.opt.demo.web.utils.HttpUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,12 +20,34 @@ public class YeekitServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         long startTime = System.currentTimeMillis();
-        LOGGER.info("开始执行翻译,当前时间:{}",startTime);
-        YeekitService yeekitService = new YeekitService();
-        for (int i=0;i<10;i++){
-            yeekitService.dotranslateEnToZh("Who am I?");
-        }
+        String forNumStr = req.getParameter("forNum");
+        //是否使用代理
+        String proxy = req.getParameter("proxy");
+        int forNum = forNumStr == null ? 1 : Integer.parseInt(forNumStr);
+        if (proxy==null) {
+            LOGGER.info("开始执行翻译,当前时间:{}", startTime);
+            YeekitService yeekitService = new YeekitService();
+            for (int i = 0; i < forNum; i++) {
+                yeekitService.dotranslateEnToZh("Who am I?");
+            }
+            long endTime = System.currentTimeMillis();
+            LOGGER.info("结束执行翻译,当前时间:{},用时:{}", endTime, (endTime - startTime));
+
+        }else
+            proxy(forNum);
+    }
+
+    /**
+     * 代理模式
+     */
+    private void proxy(int forNum){
+        long startTime = System.currentTimeMillis();
+        LOGGER.info("代理开始执行翻译,当前时间:{}", startTime);
+        //TODO..缺少地址
+        /*for (int i = 0; i < forNum; i++) {
+            HttpUtil.doGet("http:///demo-web/yees?forNum=1");
+        }*/
         long endTime = System.currentTimeMillis();
-        LOGGER.info("结束执行翻译,当前时间:{},用时:{}",endTime,(endTime-startTime));
+        LOGGER.info("代理结束执行翻译,当前时间:{},用时:{}", endTime, (endTime - startTime));
     }
 }
